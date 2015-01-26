aad63e9bbc4a8cce3c74ba23b91e6731b256ebba
Simon Wade <simon.wade@gmail.com>
2015-01-26 15:18:14 +1100

    Added ability to not import CSS

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /* jshint node: true */
 'use strict';
+var objectAssign = require('object-assign');
 
 module.exports = {
   name: 'ember-notify',
   included: function(app) {
     this._super.included(app);
-    app.import('vendor/ember-notify.css');
+    var options = objectAssign({
+      importCss: true
+    }, app.options.emberNotify);
+    if (options.importCss) app.import('vendor/ember-notify.css');
   }
 };
