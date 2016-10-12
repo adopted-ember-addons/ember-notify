5c7020fc02f6f209f3db831c21f34c6d53d5b958
Simon Wade <simon.wade@gmail.com>
2016-10-13 10:28:43 +1100

    Restore CSS

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /* jshint node: true */
 'use strict';
+var objectAssign = require('object-assign');
 
 module.exports = {
-  name: 'ember-notify'
+  name: 'ember-notify',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    var options = objectAssign({
+      importCss: true
+    }, app.options.emberNotify);
+    if (options.importCss) {
+      app.import('vendor/ember-notify.css');
+    }
+  }
 };
