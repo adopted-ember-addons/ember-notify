1d6f508a4983e1865e10be3c99281366cd0d3455
Bryan Hickerson <bryanjh@gmail.com>
2018-03-31 15:59:58 -0700

    Remove 2.4 from try. Tolerate beta failures

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,17 +2,6 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-4'
-        },
-        resolutions: {
-          'ember': 'lts-2-4'
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
