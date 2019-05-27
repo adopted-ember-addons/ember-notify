baf053a49ce2efcc9d4c893415abaafc1d6cb4b3
Bj <btecu@outlook.com>
2019-05-27 15:43:03 -0400

    Update blueprints to `3.10`

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-notify',
+  name: require('./package').name,
+
   included(app) {
     this._super.included.apply(this, arguments);
     let options = Object.assign({ importCss: true }, app.options.emberNotify);
