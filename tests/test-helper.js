da7353e9cd60d5c58e68af3819a90f7cd46408e2
Bj <btecu@outlook.com>
2019-05-27 19:48:41 -0400

    Fix tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,7 @@
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import { start } from 'ember-mocha';
 
 setApplication(Application.create(config.APP));
 
